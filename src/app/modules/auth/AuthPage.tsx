b061641f80a6b641b2c131589b23005b70334152
maidinhkhoa92 <maidinhkhoa92@gmail.com>
2023-01-29 23:47:54 +0700

    Change api url and logo

--- src/app/modules/auth/AuthPage.tsx
+++ src/app/modules/auth/AuthPage.tsx
@@ -19,8 +19,8 @@ const AuthLayout = () => {
       <div className="d-flex flex-column flex-lg-row flex-column-fluid">
         <div className="d-flex flex-lg-row-fluid">
           <div className="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
-            <img className="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="/media/auth/agency.png" alt="" />
-            <img className="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="/media/auth/agency-dark.png" alt="" />
+            <img className="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-5 mb-lg-5" src="/media/logos/default.png" alt="" />
+            <img className="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-5 mb-lg-5" src="/media/logos/default.png" alt="" />
             <h1 className="text-gray-800 fs-2qx fw-bold text-center mb-7">Fast, Efficient and Productive</h1>
             <div className="text-gray-600 fs-base text-center fw-semibold">In this kind of post,
               <a href="#" className="opacity-75-hover text-primary me-1">the blogger</a>introduces a person they’ve interviewed
