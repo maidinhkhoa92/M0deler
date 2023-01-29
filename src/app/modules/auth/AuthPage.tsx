cfca5cf47571ed45166f2f51696eef2d3cc9a158
maidinhkhoa92 <maidinhkhoa92@gmail.com>
2023-01-29 23:32:19 +0700

    Changed login page

--- src/app/modules/auth/AuthPage.tsx
+++ src/app/modules/auth/AuthPage.tsx
@@ -1,64 +1,102 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import {useEffect} from 'react'
-import {Outlet, Route, Routes} from 'react-router-dom'
-import {Registration} from './components/Registration'
-import {ForgotPassword} from './components/ForgotPassword'
-import {Login} from './components/Login'
-import {toAbsoluteUrl} from '../../../_metronic/helpers'
+import { useEffect } from 'react'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import { Registration } from './components/Registration'
+import { ForgotPassword } from './components/ForgotPassword'
+import { Login } from './components/Login'
+import { toAbsoluteUrl } from '../../../_metronic/helpers'
 
 const AuthLayout = () => {
   useEffect(() => {
     document.body.style.backgroundImage = 'none'
-    return () => {}
+    return () => { }
   }, [])
 
   return (
-    <div
-      className='d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed'
-      style={{
-        backgroundImage: `url(${toAbsoluteUrl('/media/illustrations/sketchy-1/14.png')})`,
-      }}
-    >
-      {/* begin::Content */}
-      <div className='d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20'>
-        {/* begin::Logo */}
-        <a href='#' className='mb-12'>
-          <img
-            alt='Logo'
-            src={toAbsoluteUrl('/media/logos/default.png')}
-            className='theme-dark-show h-100px'
-          />
-          <img
-            alt='Logo'
-            src={toAbsoluteUrl('/media/logos/default.png')}
-            className='theme-light-show h-100px'
-          ></img>
-        </a>
-        {/* end::Logo */}
-        {/* begin::Wrapper */}
-        <div className='w-lg-500px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto'>
-          <Outlet />
+    <div className="d-flex flex-column flex-root" id="kt_app_root" style={{
+      backgroundImage: `url(${toAbsoluteUrl('/media/auth/bg10.jpeg')})`,
+    }}>
+      <div className="d-flex flex-column flex-lg-row flex-column-fluid">
+        <div className="d-flex flex-lg-row-fluid">
+          <div className="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
+            <img className="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="/media/auth/agency.png" alt="" />
+            <img className="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="/media/auth/agency-dark.png" alt="" />
+            <h1 className="text-gray-800 fs-2qx fw-bold text-center mb-7">Fast, Efficient and Productive</h1>
+            <div className="text-gray-600 fs-base text-center fw-semibold">In this kind of post,
+              <a href="#" className="opacity-75-hover text-primary me-1">the blogger</a>introduces a person they’ve interviewed
+              <br />and provides some background information about
+              <a href="#" className="opacity-75-hover text-primary me-1">the interviewee</a>and their
+              <br />work following this is a transcript of the interview.</div>
+          </div>
         </div>
-        {/* end::Wrapper */}
+        <div className="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
+          <div className="bg-body d-flex flex-column flex-center rounded-4 w-md-600px p-10">
+            <div className="d-flex flex-center flex-column align-items-stretch h-lg-100 w-md-400px">
+              <div className="d-flex flex-center flex-column-fluid pb-15 pb-lg-20"><Outlet /></div>
+            </div>
+            <div className="d-flex flex-stack">
+              <div className="me-10">
+                <button className="btn btn-flex btn-link btn-color-gray-700 btn-active-color-primary rotate fs-base" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start" data-kt-menu-offset="0px, 0px">
+                  <img data-kt-element="current-lang-flag" className="w-20px h-20px rounded me-3" src="/media/flags/united-states.svg" alt="" />
+                  <span data-kt-element="current-lang-name" className="me-1">English</span>
+                  <span className="svg-icon svg-icon-5 svg-icon-muted rotate-180 m-0">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z" fill="currentColor" />
+                    </svg>
+                  </span>
+                </button>
+                <div className="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-4 fs-7" data-kt-menu="true" id="kt_auth_lang_menu">
+                  <div className="menu-item px-3">
+                    <a href="#" className="menu-link d-flex px-5" data-kt-lang="English">
+                      <span className="symbol symbol-20px me-4">
+                        <img data-kt-element="lang-flag" className="rounded-1" src="/media/flags/united-states.svg" alt="" />
+                      </span>
+                      <span data-kt-element="lang-name">English</span>
+                    </a>
+                  </div>
+                  <div className="menu-item px-3">
+                    <a href="#" className="menu-link d-flex px-5" data-kt-lang="Spanish">
+                      <span className="symbol symbol-20px me-4">
+                        <img data-kt-element="lang-flag" className="rounded-1" src="/media/flags/spain.svg" alt="" />
+                      </span>
+                      <span data-kt-element="lang-name">Spanish</span>
+                    </a>
+                  </div>
+                  <div className="menu-item px-3">
+                    <a href="#" className="menu-link d-flex px-5" data-kt-lang="German">
+                      <span className="symbol symbol-20px me-4">
+                        <img data-kt-element="lang-flag" className="rounded-1" src="/media/flags/germany.svg" alt="" />
+                      </span>
+                      <span data-kt-element="lang-name">German</span>
+                    </a>
+                  </div>
+                  <div className="menu-item px-3">
+                    <a href="#" className="menu-link d-flex px-5" data-kt-lang="Japanese">
+                      <span className="symbol symbol-20px me-4">
+                        <img data-kt-element="lang-flag" className="rounded-1" src="/media/flags/japan.svg" alt="" />
+                      </span>
+                      <span data-kt-element="lang-name">Japanese</span>
+                    </a>
+                  </div>
+                  <div className="menu-item px-3">
+                    <a href="#" className="menu-link d-flex px-5" data-kt-lang="French">
+                      <span className="symbol symbol-20px me-4">
+                        <img data-kt-element="lang-flag" className="rounded-1" src="/media/flags/france.svg" alt="" />
+                      </span>
+                      <span data-kt-element="lang-name">French</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="d-flex fw-semibold text-primary fs-base gap-5">
+                <a href="../../demo39/dist/pages/team.html" target="_blank">Terms</a>
+                <a href="../../demo39/dist/pages/pricing/column.html" target="_blank">Plans</a>
+                <a href="../../demo39/dist/pages/contact.html" target="_blank">Contact Us</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      {/* end::Content */}
-      {/* begin::Footer */}
-      {/* <div className='d-flex flex-center flex-column-auto p-10'>
-        <div className='d-flex align-items-center fw-semibold fs-6'>
-          <a href='#' className='text-muted text-hover-primary px-2'>
-            About
-          </a>
-
-          <a href='#' className='text-muted text-hover-primary px-2'>
-            Contact
-          </a>
-
-          <a href='#' className='text-muted text-hover-primary px-2'>
-            Contact Us
-          </a>
-        </div>
-      </div> */}
-      {/* end::Footer */}
     </div>
   )
 }
@@ -74,4 +112,4 @@ const AuthPage = () => (
   </Routes>
 )
 
-export {AuthPage}
+export { AuthPage }
--- src/app/modules/auth/components/Login.tsx
+++ src/app/modules/auth/components/Login.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import {useState} from 'react'
+import { useState } from 'react'
 import * as Yup from 'yup'
 import clsx from 'clsx'
-import {useFormik} from 'formik'
-import {getUserByToken, login} from '../core/_requests'
-import {useAuth} from '../core/Auth'
+import { useFormik } from 'formik'
+import { Link } from "react-router-dom"
+import { getUserByToken, login } from '../core/_requests'
+import { useAuth } from '../core/Auth'
 
 const loginSchema = Yup.object().shape({
   email: Yup.string()
@@ -31,17 +32,17 @@ const initialValues = {
 
 export function Login() {
   const [loading, setLoading] = useState(false)
-  const {saveAuth, setCurrentUser} = useAuth()
+  const { saveAuth, setCurrentUser } = useAuth()
   const formik = useFormik({
     initialValues,
     validationSchema: loginSchema,
-    onSubmit: async (values, {setStatus, setSubmitting}) => {
+    onSubmit: async (values, { setStatus, setSubmitting }) => {
       setLoading(true)
       try {
-        const {data: auth} = await login(values.email, values.password)
+        const { data: auth } = await login(values.email, values.password)
         console.log("auth", auth)
         saveAuth(auth)
-        const {data: user} = await getUserByToken(auth.api_token)
+        const { data: user } = await getUserByToken(auth.api_token)
         console.log("user", user)
         setCurrentUser(user)
       } catch (error) {
@@ -55,47 +56,63 @@ export function Login() {
   })
 
   return (
+
     <form
       className='form w-100'
       onSubmit={formik.handleSubmit}
       noValidate
-      id='kt_login_signin_form'
+      id='kt_sign_in_form'
     >
-      {/* begin::Heading */}
-      {/* <div className='text-center mb-10'>
-        <h1 className='text-dark mb-3'>Sign In to Metronic</h1>
-        <div className='text-gray-400 fw-bold fs-4'>
-          New Here?{' '}
-          <Link to='/auth/registration' className='link-primary fw-bolder'>
-            Create an Account
-          </Link>
+      <div className="text-center mb-11">
+        <h1 className="text-dark fw-bolder mb-3">Sign In</h1>
+        <div className="text-gray-500 fw-semibold fs-6">Your Social Campaigns</div>
+      </div>
+      <div className="row g-3 mb-9">
+        <div className="col-md-6">
+          <a
+            href="#"
+            className="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100"
+          >
+            <img
+              alt="Logo"
+              src="/media/svg/brand-logos/google-icon.svg"
+              className="h-15px me-3"
+            />Sign in with Google</a
+          >
         </div>
-      </div> */}
-      {/* begin::Heading */}
-
+        <div className="col-md-6">
+          <a
+            href="#"
+            className="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100"
+          >
+            <img
+              alt="Logo"
+              src="/media/svg/brand-logos/apple-black.svg"
+              className="theme-light-show h-15px me-3"
+            />
+            <img
+              alt="Logo"
+              src="/media/svg/brand-logos/apple-black-dark.svg"
+              className="theme-dark-show h-15px me-3"
+            />Sign in with Apple</a
+          >
+        </div>
+      </div>
+      <div className="separator separator-content my-14">
+        <span className="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
+      </div>
       {formik.status && (
         <div className='mb-lg-15 alert alert-danger'>
           <div className='alert-text font-weight-bold'>{formik.status}</div>
         </div>
       )}
-      {/* ) : (
-        <div className='mb-10 bg-light-info p-8 rounded'>
-          <div className='text-info'>
-            Use account <strong>admin@demo.com</strong> and password <strong>demo</strong> to
-            continue.
-          </div>
-        </div>
-      )} */}
-
-      {/* begin::Form group */}
-      <div className='fv-row mb-10'>
-        <label className='form-label fs-6 fw-bolder text-dark'>Email</label>
+      <div className="fv-row mb-8">
         <input
           placeholder='Email'
           {...formik.getFieldProps('email')}
           className={clsx(
-            'form-control form-control-lg form-control-solid',
-            {'is-invalid': formik.touched.email && formik.errors.email},
+            'form-control bg-transparent',
+            { 'is-invalid': formik.touched.email && formik.errors.email },
             {
               'is-valid': formik.touched.email && !formik.errors.email,
             }
@@ -110,32 +127,14 @@ export function Login() {
           </div>
         )}
       </div>
-      {/* end::Form group */}
-
-      {/* begin::Form group */}
-      <div className='fv-row mb-10'>
-        <div className='d-flex justify-content-between mt-n5'>
-          <div className='d-flex flex-stack mb-2'>
-            {/* begin::Label */}
-            <label className='form-label fw-bolder text-dark fs-6 mb-0'>Password</label>
-            {/* end::Label */}
-            {/* begin::Link */}
-            {/* <Link
-              to='/auth/forgot-password'
-              className='link-primary fs-6 fw-bolder'
-              style={{marginLeft: '5px'}}
-            >
-              Forgot Password ?
-            </Link> */}
-            {/* end::Link */}
-          </div>
-        </div>
+      <div className="fv-row mb-3">
         <input
+          placeholder="Password"
           type='password'
           autoComplete='off'
           {...formik.getFieldProps('password')}
           className={clsx(
-            'form-control form-control-lg form-control-solid',
+            'form-control bg-transparent',
             {
               'is-invalid': formik.touched.password && formik.errors.password,
             },
@@ -152,28 +151,31 @@ export function Login() {
           </div>
         )}
       </div>
-      {/* end::Form group */}
-
-      {/* begin::Action */}
-      <div className='text-center'>
-        <button
-          type='submit'
-          id='kt_sign_in_submit'
-          className='btn btn-lg btn-primary w-100 mb-5'
-          disabled={formik.isSubmitting || !formik.isValid}
+      <div className="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+        <div></div>
+        <a
+          href="../../demo39/dist/authentication/layouts/overlay/reset-password.html"
+          className="link-primary"
+        >Forgot Password ?</a
         >
-          {!loading && <span className='indicator-label'>Continue</span>}
-          {loading && (
-            <span className='indicator-progress' style={{display: 'block'}}>
-              Please wait...
-              <span className='spinner-border spinner-border-sm align-middle ms-2'></span>
-            </span>
-          )}
-        </button>
-
-        
       </div>
-      {/* end::Action */}
+      <div className="d-grid mb-10">
+        <button type="submit" id="kt_sign_in_submit" className="btn btn-primary">
+          <span className="indicator-label">Sign In</span>
+          <span className="indicator-progress"
+          >Please wait...
+            <span className="spinner-border spinner-border-sm align-middle ms-2"></span
+            ></span>
+        </button>
+      </div>
+      <div className="text-gray-500 text-center fw-semibold fs-6">
+        Not a Member yet?
+        <a
+          href="../../demo39/dist/authentication/layouts/overlay/sign-up.html"
+          className="link-primary"
+        >Sign up</a
+        >
+      </div>
     </form>
   )
 }
