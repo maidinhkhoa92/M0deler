f2d8239d7c0f66ce1994548797178bff9978efd1
maidinhkhoa92 <maidinhkhoa92@gmail.com>
2023-01-30 10:52:55 +0700

    fixed text

--- src/app/pages/chart/histogram-balance.tsx
+++ src/app/pages/chart/histogram-balance.tsx
@@ -61,7 +61,7 @@ const Histograms: React.FC<Props> = ({className}) => {
     <div className={`card ${className}`}>
       <div className='card-header border-0 pt-5'>
         <h3 className='card-title align-items-start flex-column'>
-          <span className='card-label fw-bold fs-3 mb-1'>Histogram of Ending Account Balances Data</span>
+          <span className='card-label fw-bold fs-3 mb-1'>Histogram of Ending Account Balances</span>
         </h3>
       </div>
       <div className='card-body'>
